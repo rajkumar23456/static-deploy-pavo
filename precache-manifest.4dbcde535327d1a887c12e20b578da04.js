4fde0533ff7874040465a32b3dc697061e982c6b
rajkumar23456 <23kittu23@gmail.com>
2021-05-20 15:55:49 +0530

    Updates

---
+++ precache-manifest.4dbcde535327d1a887c12e20b578da04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b090f9a26dffd39818999fd886bfc92f",
+    "url": "/static-deploy-pavo/index.html"
+  },
+  {
+    "revision": "bde28e3d0e8c63a0b8d8",
+    "url": "/static-deploy-pavo/static/css/main.d8f5e53f.chunk.css"
+  },
+  {
+    "revision": "a1f4f216db3e2ef69a13",
+    "url": "/static-deploy-pavo/static/js/2.295043b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static-deploy-pavo/static/js/2.295043b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde28e3d0e8c63a0b8d8",
+    "url": "/static-deploy-pavo/static/js/main.50632e76.chunk.js"
+  },
+  {
+    "revision": "be80a141254fd04eef12",
+    "url": "/static-deploy-pavo/static/js/runtime-main.29b02a70.js"
+  }
+]);
